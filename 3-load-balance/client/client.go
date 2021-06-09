0d23d3eb0b81e6af617900c4b9520f6b68540375
xiuxiubiu <xiuxiubiu@gmail.com>
2021-06-09 12:51:26 +0800

    gofmt

--- 3-load-balance/client/client.go
+++ 3-load-balance/client/client.go
@@ -12,18 +12,18 @@ import (
 )
 
 func main() {
-	
+
 	cli, err := clientv3.New(clientv3.Config{Endpoints: []string{"127.0.0.1:2379"}})
 	if err != nil {
 		fmt.Printf("clientv3 new err: %v", err)
 		return
 	}
-	
+
 	resolver.Register(NewBuilder(cli))
-	
-	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	
+
 	conn, err := grpc.DialContext(
 		ctx,
 		"etcd:///test",
@@ -31,26 +31,25 @@ func main() {
 		grpc.WithInsecure(),
 		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "weighted_round_robin"}`),
 	)
-	
+
 	if err != nil {
 		fmt.Printf("grpc DialContext err: %v", err)
 		return
 	}
-	
+
 	bc := proto.NewBalanceClient(conn)
-	
+
 	for i := 0; i < 100; i++ {
-		
+
 		_, err := bc.Call(context.Background(), &proto.CallRequest{Num: int32(i)})
 		if err != nil {
 			fmt.Printf("NewBalanceClient call err: %v\n", err)
 			return
 		}
-		
+
 		fmt.Printf("call num: %d\n", i)
 	}
 }
-
 
 type Builder struct {
 	client *clientv3.Client
@@ -65,9 +64,9 @@ func NewBuilder(client *clientv3.Client) *Builder {
 func (b *Builder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 
 	r := &EtcdResolver{
-		c: b.client,
-		target: target.Endpoint,
-		cc: cc,
+		c:       b.client,
+		target:  target.Endpoint,
+		cc:      cc,
 		address: map[string]struct{}{},
 	}
 	r.start()
@@ -79,20 +78,20 @@ func (b *Builder) Scheme() string {
 }
 
 type EtcdResolver struct {
-	c *clientv3.Client
-	target string
-	cc resolver.ClientConn
+	c       *clientv3.Client
+	target  string
+	cc      resolver.ClientConn
 	address map[string]struct{}
 }
 
 func (er *EtcdResolver) start() {
-	
+
 	resp, _ := er.c.Get(context.Background(), "/etcd3_naming", clientv3.WithPrefix())
 	for _, kv := range resp.Kvs {
 		er.address[string(kv.Value)] = struct{}{}
 	}
 	er.ResolveNow(resolver.ResolveNowOptions{})
-	
+
 	go func() {
 		wc := er.c.Watch(context.Background(), "/etcd3_naming", clientv3.WithPrefix())
 		for m := range wc {
@@ -118,5 +117,5 @@ func (er *EtcdResolver) ResolveNow(options resolver.ResolveNowOptions) {
 }
 
 func (er *EtcdResolver) Close() {
-	
+
 }
--- 3-load-balance/server/server.go
+++ 3-load-balance/server/server.go
@@ -11,21 +11,20 @@ import (
 	"net"
 )
 
-
 func main() {
-	
+
 	port := flag.Int("port", 8080, "port")
 	flag.Parse()
-	
+
 	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
 	if err != nil {
 		panic(err)
 	}
-	
+
 	if err := register(context.Background(), "hello", "127.0.0.1", *port, []string{"127.0.0.1:2379"}, 1); err != nil {
 		panic(err)
 	}
-	
+
 	s := grpc.NewServer()
 	proto.RegisterBalanceServer(s, &server{})
 	reflection.Register(s)
@@ -42,44 +41,44 @@ func (s *server) Call(ctx context.Context, request *proto.CallRequest) (*proto.C
 }
 
 type registerInfo struct {
-	client *clientv3.Client
+	client  *clientv3.Client
 	leaseId clientv3.LeaseID
 }
 
 var ri registerInfo
 var prefix = "etcd3_naming"
 
-func register(ctx context.Context, name , host string, port int, endpoints []string, ttl int) error {
-	
+func register(ctx context.Context, name, host string, port int, endpoints []string, ttl int) error {
+
 	ri = registerInfo{}
-	
+
 	serviceValue := fmt.Sprintf("%s:%d", host, port)
 	serviceKey := fmt.Sprintf("/%s/%s/%s", prefix, name, serviceValue)
-	
+
 	cli, err := clientv3.New(clientv3.Config{Endpoints: endpoints})
 	if err != nil {
 		fmt.Printf("etcd client v3 new err: %v", err)
 		return err
 	}
 	fmt.Printf("etcd client new: %v", endpoints)
-	
+
 	ri.client = cli
-	
+
 	lease, err := cli.Grant(ctx, int64(ttl))
 	if err != nil {
 		fmt.Printf("etcd client v3 grant err: %v\n", err)
 		return err
 	}
 	fmt.Printf("etcd grant lease: %v\n", lease)
-	
+
 	ri.leaseId = lease.ID
-	
+
 	if _, err = cli.Put(ctx, serviceKey, serviceValue, clientv3.WithLease(lease.ID)); err != nil {
 		fmt.Printf("etcd client v3 put err: %v\n", err)
 		return err
 	}
 	fmt.Printf("etcd put key: %v, value: %v\n", serviceKey, serviceValue)
-	
+
 	go func() {
 		kc, err := cli.KeepAlive(ctx, lease.ID)
 		if err != nil {
@@ -87,11 +86,11 @@ func register(ctx context.Context, name , host string, port int, endpoints []str
 			return
 		}
 		fmt.Printf("etcd keep alive %v\n", lease.ID)
-		
-		for  {
-			<- kc
+
+		for {
+			<-kc
 		}
 	}()
-	
+
 	return nil
 }
